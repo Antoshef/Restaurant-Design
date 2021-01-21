7bee1a59f1b9e981cdd0012af426ef6a912959cd
Antoshef <antoshef21@gmail.com>
2021-01-21 17:24:07 +0200

    Restaurant

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 
 let nav = document.getElementsByClassName('e-nav')[0];
+let header = document.getElementsByClassName('c-header')[0];
+window.onscroll = function() {scroll()};
+let sticky = header.offsetTop;
 
 function openMenu() {
     nav.style.left = '0';
@@ -8,3 +11,11 @@ function openMenu() {
 function closeMenu() {
     nav.style.left = '-100%'
 }
+
+function scroll() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add('sticky');
+    } else {
+        header.classList.remove('sticky');
+    };
+};
